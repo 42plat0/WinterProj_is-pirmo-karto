3bfe85b54fed936fea88cda2a25e2d325be0abe4
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-02-08 21:12:56 +0200

    Add functionality for mobile side menu

--- header.js
+++ header.js
@@ -1,4 +1,12 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display= 'flex'
+
+/* Function for hamburger menu, when it's clicked the side menu shows up */
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+/* Function for slide menu close icon, when it's clicked the side menu disappears */
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
