eefabf90728b4e96b519176e42fd8c3bd72bd63e
Gypra <gytisnaujasis@icloud.com>
2025-02-08 11:25:15 +0200

    Add email validation, on focus remove error stylings on fields

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,9 +1,20 @@
 const form = document.getElementById("contact-us-form");
 
+// Loop through form elements except button
+for (let i = 0; i < form.length - 1; i++) {
+    form[i].addEventListener("focus", () => {
+        // Restore fields's styles
+        form[i].style.color = "#FFF";
+        form[i].style["border-bottom"] = "1px solid #79c8c7";
+        // Stop displaying err text
+        form[i].nextElementSibling.style.display = "none";
+    });
+}
+
 // Use standart event listener
 // novalidate attribute is present on form
 form.addEventListener("submit", (event) => {
-    // Disable default behavior of event
+    // Stop form from submiting
     event.preventDefault();
 
     // Get values
@@ -13,45 +24,51 @@ form.addEventListener("submit", (event) => {
         titleVal = form["title"].value,
         msgVal = form["message"].value;
 
-    // Check if all fields are filled
-    let validInput = nameVal && emailVal && compNameVal && titleVal && msgVal;
+    // Empty fields's index in children array is extracted
+    // And used to style them in else clause
+    let input = [
+        Boolean(nameVal),
+        Boolean(validateEmail(emailVal)),
+        Boolean(compNameVal),
+        Boolean(titleVal),
+        Boolean(msgVal),
+    ];
+    let indexesOfInvalidInp = getAllIndexes(input, false);
 
-    console.log(form.children);
-    if (validInput) {
+    if (!indexesOfInvalidInp.length) {
+        // Clear fields
         form.reset();
-        for (let i = 0; i < form.children.length - 1; i+= 2){
-            // Restore fields to starting styles
+        // Loop through children except button
+        for (let i = 0; i < form.children.length - 1; i += 2) {
+            // Restore fields's styles
             form.children[i].style.color = "#FFF";
             form.children[i].style["border-bottom"] = "1px solid #79c8c7";
 
-            // Remove text under fields
-            form.children[(i + 1)].style.display = "none";
+            // Remove text under field
+            form.children[i + 1].style.display = "none";
         }
     } else {
-        if (!nameVal) {
-            form.children[0].style.color = "#f67e7e";
-            form.children[0].style["border-bottom"] = "1px solid #f67e7e";
-            form.children[0].nextElementSibling.style.display = "block";
-        }
-        if (!emailVal){
-            form.children[2].style.color = "#f67e7e";
-            form.children[2].style["border-bottom"] = "1px solid #f67e7e";
-            form.children[2].nextElementSibling.style.display = "block";
-        }
-        if (!compNameVal){
-            form.children[4].style.color = "#f67e7e";
-            form.children[4].style["border-bottom"] = "1px solid #f67e7e";
-            form.children[4].nextElementSibling.style.display = "block";
-        }
-        if (!titleVal){
-            form.children[6].style.color = "#f67e7e";
-            form.children[6].style["border-bottom"] = "1px solid #f67e7e";
-            form.children[6].nextElementSibling.style.display = "block";
-        }
-        if (!msgVal){
-            form.children[8].style.color = "#f67e7e";
-            form.children[8].style["border-bottom"] = "1px solid #f67e7e";
-            form.children[8].nextElementSibling.style.display = "block";
-        }
+        indexesOfInvalidInp.forEach((ind) => {
+            form.children[ind].style.color = "#f67e7e";
+            form.children[ind].style["border-bottom"] = "1px solid #f67e7e";
+            // Display text under field
+            form.children[ind].nextElementSibling.style.display = "block";
+        });
     }
 });
+
+function getAllIndexes(inpArr, val) {
+    let arr = [];
+    for (let i = 0; i < inpArr.length; i++) {
+        if (inpArr[i] === val) arr.push(i * 2);
+    }
+    return arr;
+}
+
+function validateEmail (email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
